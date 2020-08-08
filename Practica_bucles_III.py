c9db537ea8fa84deccc176c822fb821f2de31501
ericksonvergara <ericksonvergararojas@hotmail.com>
2020-08-08 00:24:05 -0500

    curso_python_vsc

---
+++ Practica_bucles_III.py
@@ -0,0 +1,13 @@
+
+# la funcion de tipo f nos permite juagar con formatos de forma diferente, y nos va permitir concatenar.
+
+for i in range(5):
+    print(f"valor de la variable {i}")
+
+print("--------------------------------------------")
+
+#El tipo rage permite hasta 3 argumentos. siendo el primer argumento el numero inicial,
+#el segundo argumento hasta donde llega y el tercer argumento de cuanto en cuanto da el salto.
+
+for x in range(10, 100, 2):
+    print(f"valor de la variable {x}")
